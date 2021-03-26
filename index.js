2567ed69e10279b7d41103d8244e6372f05f09ba
Daniel Beal <dbeal@wiser.com>
2021-03-26 13:07:31 -0700

    add helper log message

--- index.js
+++ index.js
@@ -11,22 +11,33 @@ async function run(callback) {
     excludeList.sort();
     const orphanTime = core.getInput('orphan-time');
 
-    const dry = core.getInput('dry') == false;
+    const executeDelete = core.getInput('dry') == false;
 
     const token = core.getInput('token');
 
     const octokit = github.getOctokit(token);
 
-    const allBranches = _.sortBy(await octokit.repos.listBranches({
-      ...github.context.repo
-    }), 'name');
+    console.log('configured excluding branches:', excludeList);
 
-    const allOpenPRBranches = _.map(octokit.pulls.list({
+    const allBranches = _.sortBy((await octokit.repos.listBranches({
       ...github.context.repo,
-      state: 'open'
-    }), 'base.ref');
+      per_page: 100
+      
+    })).data, 'name');
+
+    const allOpenPRBranches = _.map((await octokit.pulls.list({
+      ...github.context.repo,
+      state: 'open',
+      per_page: 100
+    })).data, 'head.ref');
+
+    if(allOpenPRBranches.length >= 100) {
+      throw new Error('too many PRs open (>100), please close some.');
+    }
 
     allOpenPRBranches.sort();
+
+    console.log('all open PR branches:', allOpenPRBranches);
 
     // conditions on which a branch ref is deleted:
     const allOrphanBranches = _.chain(allBranches)
@@ -37,25 +48,29 @@ async function run(callback) {
     // * branch has not seen commit in some time (say a month)
     const oldestAcceptedTime = Date.now() - orphanTime;
 
+    console.log('checking', allOrphanBranches.length, 'orphan branches...');
+
     const deletedBranches = [];
     for(const b of allOrphanBranches) {
-      const commitInfo = await octokit.repos.getCommit({ 
+      const commitInfo = (await octokit.repos.getCommit({ 
         ...github.context.repo,
         ref: b.name }
-      );
+      )).data;
 
       if( new Date(commitInfo.commit.author.date).getTime() < oldestAcceptedTime) {
         console.log('[DELETE]', b.name);
 
-        if(!dry) {
-          console.log('actually delete');
-          /*await octokit.git.deleteRef({
+        if(executeDelete) {
+          await octokit.git.deleteRef({
             ...github.context.repo,
             ref: 'refs/heads/' + b.name
-          });*/
+          });
         }
 
         deletedBranches.push(b.name);
+      }
+      else {
+        console.log('[KEEP]', b.name);
       }
     }
 
